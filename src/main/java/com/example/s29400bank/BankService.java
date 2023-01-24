a2c43630ad8f19e3536e47fcb9717be8c97d5893
Antoni Kania <antek.kania@wp.pl>
2023-01-24 21:47:04 +0100

    API added

--- src/main/java/com/example/s29400bank/BankService.java
+++ src/main/java/com/example/s29400bank/BankService.java
@@ -1,9 +1,14 @@
 package com.example.s29400bank;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+@RestController
 @Component
 public class BankService {
     private final UserStorage userStorage;
@@ -12,7 +17,13 @@ public class BankService {
         this.userStorage = userStorage;
     }
 
-    public UserInfo getUserInfo(Integer clientId) {
+    @GetMapping("/")
+    public String returnStatus() {
+        return "ok";
+    }
+
+    @GetMapping("/user/{clientId}")
+    public UserInfo getUserInfo(@PathVariable Integer clientId) {
         Optional<User> user = userStorage.findUserById(clientId);
         if (user.isEmpty()) {
             return null;
@@ -20,13 +31,15 @@ public class BankService {
         return new UserInfo(user.get().getName(), user.get().getBalance());
     }
 
-    public Integer registerClient(String clientName, Integer balance) {
+    @PostMapping("/user/{clientName}/{balance}")
+    public Integer registerClient(@PathVariable String clientName, @PathVariable Integer balance) {
         Integer id = userStorage.getUserList().size() + 1;
         userStorage.addUser(new User(id, clientName, balance));
         return id;
     }
 
-    public OrderInfo depositOrder(Integer clientId, Integer amount) {
+    @PostMapping("/deposit/{clientId}/{amount}")
+    public OrderInfo depositOrder(@PathVariable Integer clientId, @PathVariable Integer amount) {
         Optional<User> user = userStorage.findUserById(clientId);
         if (user.isEmpty()) {
             return new OrderInfo(Status.DECLINE, 0, "Client don't exist in database");
@@ -36,14 +49,18 @@ public class BankService {
         return new OrderInfo(Status.ACCEPTED, user.get().getBalance(), "");
     }
 
-    public OrderInfo transferOrder(Integer clientId, Integer amount) {
+    @PostMapping("/transfer/{clientId}/{amount}")
+    public OrderInfo transferOrder(@PathVariable Integer clientId, @PathVariable Integer amount) {
         Optional<User> user = userStorage.findUserById(clientId);
+
         if (user.isEmpty()) {
             return new OrderInfo(Status.DECLINE, 0, "Client don't exist in database");
         }
+
         if (user.get().getBalance() < amount) {
             return new OrderInfo(Status.DECLINE, 0, "Lack of funds");
         }
+
         user.get().decreaseBalance(amount);
         return new OrderInfo(Status.ACCEPTED, user.get().getBalance(), "");
     }
